b50afb6ea5e02504f1a0db63f100e39b2bf7034d
Tony Weidel <8aweidel@stpatrick.org>
2017-05-03 14:06:08 -0500

    lol only password gui now

--- string.js
+++ string.js
@@ -6,32 +6,32 @@ function PasswordMessage(m){
 }
 
 function inRange(char,min,max){
-    let unicode = char.charCodeAt(0);
-    if(unicode>=min && unicode<=max){
-      return true;
-    }
-    else{
-      return false;
-    }
+  let unicode = char.charCodeAt(0);
+  if(unicode>=min && unicode<=max){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 exports.checkLength = function(str){
-    let valid = (str.length >= 8 && str.length <= 20);
-    try{
-        if(str.length < 8){
-            throw new PasswordMessage("Your password is too short. Should be at least 8 letters.");
-        }
-        else if(str.length > 20){
-            throw new PasswordMessage("Your password is too long. Should be at most 20 letters.");
-        }
-        else{
-            throw new PasswordMessage("Your password is an appropriate length.");
-        }
+  let valid = (str.length >= 8 && str.length <= 20);
+  try{
+    if(str.length < 8){
+      throw new PasswordMessage("Your password is too short. Should be at least 8 letters.");
     }
-    catch(e){
-        console.log(e.name+": "+e.message);
-        return valid;
+    else if(str.length > 20){
+      throw new PasswordMessage("Your password is too long. Should be at most 20 letters.");
     }
+    else{
+      throw new PasswordMessage("Your password is an appropriate length.");
+    }
+  }
+  catch(e){
+    console.log(e.name+": "+e.message);
+    return valid;
+  }
 }
 
 
@@ -44,7 +44,7 @@ exports.containsUpper =function(str){
       }
     }
     if(hasUpper = true){
-    throw new PasswordMessage("Password contains at least one uppercase letter");
+      throw new PasswordMessage("Password contains at least one uppercase letter");
     }
     else{
       throw new PasswordMessage("Password must contain at least one uppercase letter");
@@ -64,7 +64,7 @@ exports.containsLower =function(str){
       }
     }
     if(hasLower = true){
-    throw new PasswordMessage("Password contains at least one lowercase letter");
+      throw new PasswordMessage("Password contains at least one lowercase letter");
     }
     else{
       throw new PasswordMessage("Password must contain at least one lowercase letter");
@@ -84,7 +84,7 @@ exports.containsNumerical =function(str){
       }
     }
     if(hasNumber = true){
-    throw new PasswordMessage("Password contains at least one number");
+      throw new PasswordMessage("Password contains at least one number");
     }
     else{
       throw new PasswordMessage("Password must contain at least one number");
@@ -97,20 +97,22 @@ exports.containsNumerical =function(str){
 
 exports.containsSpecial =function(str){
   let hasSpecial = false;
+  special = [33,35,36,37,38,42,64,94];
   try{
-    for(let m=0; m<string.length; m++){
-      if(inRange(str[m],65,90)==true){
-        hasUpper=true;
+    for(let s=0; s<special.length; s++){
+      for(let m=0; m<string.length; m++){
+        if(str[m]==special[s]){
+          hasSpecial = true;
+          throw new PasswordMessage("Password contains at least one special character");
+        }
       }
     }
-    if(hasUpper = true){
-    throw new PasswordMessage("Password contains at least one uppercase letter");
-    }
-    else{
-      throw new PasswordMessage("Password must contain at least one uppercase letter");
+    if(hasSpecial = false){
+      throw new PasswordMessage("Password must contain at least one special character");
     }
   }
   catch(e){
     console.log(e.name+": "+e.message);
+    return hasSpecial;
   }
 }
